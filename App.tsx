bd1599f7df827384468f44fce09c524862d30aa7
glaubermatos <glaub.oliveira@hotmail.com>
2023-07-28 09:23:40 -0300

    config styled-components, phospho-icons, expo-font, expo-google-fonts-nunito e module-resolver

--- App.tsx
+++ App.tsx
@@ -1,14 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , ActivityIndicator} from 'react-native';
+
+import { Nunito_400Regular, Nunito_700Bold, useFonts } from '@expo-google-fonts/nunito'
+
+import * as Icon from 'phosphor-react-native' 
+
+import defaultTheme from './src/theme/defaultTheme';
+import { styled } from 'styled-components/native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_400Regular,
+    Nunito_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <Container>
+      <Icon.AngularLogo weight='fill' size={64} color={defaultTheme.COLORS.RED_DARK} />
+      <Text style={{fontSize: defaultTheme.FONT_SIZE._2XL, fontFamily: defaultTheme.FONT_FAMILY.BOLD}}>Glauber Matos</Text>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
+
+const Container = styled.View`
+  flex: 1;
+  background-color: '#fff';
+  align-items: center;
+  justify-content: center;
+`
 
 const styles = StyleSheet.create({
   container: {
